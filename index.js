71f2d0593b425044a01b8df9cdac9fafbaf584db
sbarik1997 <126463628+sbarik1997@users.noreply.github.com>
2023-02-27 16:14:27 +0530

    updated student class

--- index.js
+++ index.js
@@ -1,7 +1,22 @@
-creating a array and traversing the array by for loop
+//creating a array and traversing the array by for loop
 
 let arr = [1,2,5,7,9,12];
 
 for(let i = 0;i < arr.length;i++){
 console.log(arr[i]);
 }
+
+//Making a class of student 
+
+class Students{
+  constructor(name,Class,roll_no,age){
+    this.name = name;
+    this.Class = Class;
+    this.roll_no = roll_no;
+    this.age = age;
+  }
+}
+
+let student1 = new Students("Ashis Das",11,1,17);
+console.log(student1);
+  
